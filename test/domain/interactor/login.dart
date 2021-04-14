33dd6ad0cb00bc4fe396040e89e9d43e5069b84f
Maksym Kozachenko <maksKozachenko2@gmail.com>
2021-04-14 22:15:06 +0300

    Test dependencies and structure

---
+++ test/domain/interactor/login.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void login() {
+  group('Login tests', () {
+    test('Sign in', () async {});
+  });
+}
---
+++ test/main_test.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'domain/interactor/login.dart';
+
+void main() {
+  setUp(() async {});
+  login();
+  tearDown(() async {});
+}
