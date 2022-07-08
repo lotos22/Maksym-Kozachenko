7eb370fa07818023216ba399c96a109dd516b98f
Maksym Kozachenko <maksKozachenko2@gmail.com>
2022-07-09 01:12:14 +0300

    Refactor structure

--- lib/domain/use_case.dart
+++ lib/domain/use_case.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:toptal_test/domain/one_of.dart';
 import 'package:toptal_test/domain/repository/failure.dart';
 
 abstract class UseCase<Type, Params> {
-  @protected
   Future<OneOf<Failure, Type>> run(Params params);
   Future execute(Params params, Function(OneOf<Failure, Type>) onResult) async {
     try {
